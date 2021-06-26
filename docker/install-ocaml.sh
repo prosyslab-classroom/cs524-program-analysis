3255169d1f2ab422138193ac9507ff50e73a1a6b
Kihong Heo <kihong.heo@gmail.com>
2021-06-26 22:51:34 +0900

    Update install-ocaml.sh

--- docker/install-ocaml.sh
+++ docker/install-ocaml.sh
@@ -8,3 +8,5 @@ opam init --compiler=$OCAML_VERSION --disable-sandboxing && \
   opam switch create $OPAM_SWITCH $OCAML_VERSION && \
   eval $(opam env) && \
   opam install utop dune llvm.10.0.0 ounit merlin ocamlformat
+
+opam pin add git@github.com:prosyslab-classroom/llvm-utils
