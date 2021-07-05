475201fcdbeaba03d3d83c81015fb05d96869638
kangwoosukeq <kangwoosuk1@gmail.com>
2021-07-05 21:44:02 +0900

    Update install-ocaml.sh for ocaml-lsp-server

--- docker/install-ocaml.sh
+++ docker/install-ocaml.sh
@@ -7,7 +7,7 @@ export OPAMYES=1
 opam init --compiler=$OCAML_VERSION --disable-sandboxing
 opam switch create $OPAM_SWITCH $OCAML_VERSION
 eval $(opam env)
-opam install utop dune llvm.10.0.0 ounit merlin ocamlformat
+opam install utop dune llvm.10.0.0 ounit merlin ocamlformat ocaml-lsp-server
 opam pin add git+https://github.com/prosyslab-classroom/llvm-utils.git
 
 echo "$(opam env)" >> ~/.bashrc
