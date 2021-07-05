1bedc19dc57bc4090917bea4385bbbfeaa29e718
kangwoosukeq <kangwoosuk1@gmail.com>
2021-07-05 15:52:17 +0900

    Update files for docker

--- docker/install-ocaml.sh
+++ docker/install-ocaml.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-OCAML_VERSION=4.11.0
-OPAM_SWITCH=prosyslab-classroom-$OCAML_VERSION
-OPAMYES=1
+export OCAML_VERSION=4.11.0
+export OPAM_SWITCH=prosyslab-classroom-$OCAML_VERSION
+export OPAMYES=1
 
-opam init --compiler=$OCAML_VERSION --disable-sandboxing && \
-  opam switch create $OPAM_SWITCH $OCAML_VERSION && \
-  eval $(opam env) && \
-  opam install utop dune llvm.10.0.0 ounit merlin ocamlformat
+opam init --compiler=$OCAML_VERSION --disable-sandboxing
+opam switch create $OPAM_SWITCH $OCAML_VERSION
+eval $(opam env)
+opam install utop dune llvm.10.0.0 ounit merlin ocamlformat
+opam pin add git+https://github.com/prosyslab-classroom/llvm-utils.git
 
-opam pin add git@github.com:prosyslab-classroom/llvm-utils
+echo "$(opam env)" >> ~/.bashrc
