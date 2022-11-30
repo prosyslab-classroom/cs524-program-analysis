c7dc521383d60683f4b14bca02100025d4c797df
Kihong Heo <kihong.heo@kaist.ac.kr>
2022-12-01 00:34:04 +0900

    add gallery-gen

---
+++ script/gallery-gen.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import getpass
+import re
+
+from github import Github
+
+cols = 5
+
+
+def print_line():
+    line = "|"
+    for i in range(cols):
+        line += ":-:|"
+    return line
+
+
+def main():
+    username = input("Username: ")
+    password = getpass.getpass()
+    g = Github(username, password)
+    org = g.get_organization("prosyslab-classroom")
+    repo = org.get_repo("cs524-program-analysis")
+    issues = repo.get_issues(labels=["art competition"])
+    index = 0
+    line = "|"
+    line_printed = False
+    for issue in issues:
+        title = issue.title.split("] ")[1]
+        print(title)
+        issue_url = issue.url.replace("api.", "").replace("/repos/", "/")
+        image_url = re.search(r"https.+(png|jpg)", issue.body).group(0)
+        user = issue.user.login
+        user_url = issue.user.url
+        line += f"[![{title}]({image_url})]({issue_url}){title}<br>by [{user}]({user_url})|"
+        index += 1
+        if index % cols == 0:
+            if not line_printed:
+                line += "\n"
+                line_printed = True
+                line += print_line()
+            line += "\n|"
+    print(line)
+
+
+if __name__ == "__main__":
+    main()
