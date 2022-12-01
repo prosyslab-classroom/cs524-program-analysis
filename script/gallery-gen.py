508364e2dc13611263a7d45fc96b82387e9c1905
Kihong Heo <kihong.heo@gmail.com>
2022-12-01 18:09:44 +0900

    update

--- script/gallery-gen.py
+++ script/gallery-gen.py
@@ -27,12 +27,20 @@ def main():
     line_printed = False
     for issue in issues:
         title = issue.title.split("] ")[1]
-        print(title)
         issue_url = issue.url.replace("api.", "").replace("/repos/", "/")
         image_url = re.search(r"https.+(png|jpg)", issue.body).group(0)
         user = issue.user.login
         user_url = issue.user.url
-        line += f"[![{title}]({image_url})]({issue_url}){title}<br>by [{user}]({user_url})|"
+        (gold, silver, bronze) = (False, False, False)
+        for label in issue.labels:
+            if label.name == "gold":
+                gold = True
+            elif label.name == "silver":
+                silver = True
+            elif label.name == "bronze":
+                bronze = True
+        medal = ":1st_place_medal:" if gold else ":2nd_place_medal:" if silver else ":3rd_place_medal:" if bronze else ""
+        line += f"[![{title}]({image_url})]({issue_url}){title} {medal}<br>by [{user}]({user_url})|"
         index += 1
         if index % cols == 0:
             if not line_printed:
